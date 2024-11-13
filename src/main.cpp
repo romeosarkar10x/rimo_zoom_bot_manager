bab31d310fad359e43d5af0ace380e4b8a02b949
Romeo Sarkar <romeosarkar10x@gmail.com>
2024-11-13 17:19:53 +0530

    updated cmakelists.txt and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,25 +13,21 @@
 using tcp = boost::asio::ip::tcp;    // from <boost/asio.hpp>
 namespace http = boost::beast::http; // from <boost/beast/http.hpp>
 
-namespace my_program_state
+std::size_t request_count()
 {
-    std::size_t requestcount()
-    {
-        static std::size_t count = 0;
-        return ++count;
-    }
+    static std::size_t count = 0;
+    return ++count;
+}
 
-    std::time_t now()
-    {
-        return std::time(0);
-    }
-} // namespace my_program_state
-
-class http_connection : public std::enable_shared_from_this<http_connection>
+std::time_t now()
 {
-public:
+    return std::time(0);
+}
+
+struct http_connection : public std::enable_shared_from_this<http_connection>
+{
     http_connection(tcp::socket socket) :
-        socket_(std::move(socket))
+        m_socket(std::move(socket))
     {}
 
     // Initiate the asynchronous operations associated with the connection.
@@ -43,27 +39,27 @@ public:
 
 private:
     // The socket for the currently connected client.
-    tcp::socket socket_;
+    tcp::socket m_socket;
 
     // The buffer for performing reads.
-    boost::beast::flat_buffer buffer_ { 8192 };
+    boost::beast::flat_buffer m_buffer { 8192 };
 
     // The request message.
-    http::request<http::dynamic_body> request;
+    http::request<http::dynamic_body> m_request;
 
     // The response message.
-    http::response<http::dynamic_body> response;
+    http::response<http::dynamic_body> m_response;
 
     // The timer for putting a deadline on connection processing.
-    boost::asio::basic_waitable_timer<std::chrono::steady_clock> deadline_ { socket_.get_executor(),
-                                                                             std::chrono::seconds(60) };
+    boost::asio::basic_waitable_timer<std::chrono::steady_clock> m_deadline { m_socket.get_executor(),
+                                                                              std::chrono::seconds(60) };
 
     // Asynchronously receive a complete request message.
     void read_request()
     {
         auto self = shared_from_this();
 
-        http::async_read(socket_, buffer_, request,
+        http::async_read(m_socket, m_buffer, m_request,
                          [self](boost::beast::error_code ec, std::size_t bytes_transferred)
                          {
                              boost::ignore_unused(bytes_transferred);
@@ -75,24 +71,26 @@ private:
     // Determine what needs to be done with the request message.
     void process_request()
     {
-        response.version(request.version());
-        response.keep_alive(false);
+        m_response.version(m_request.version());
+        m_response.keep_alive(false);
 
-        switch(request.method())
+        switch(m_request.method())
         {
             case http::verb::get:
-                response.result(http::status::ok);
-                response.set(http::field::server, "Beast");
+                m_response.result(http::status::ok);
+                // response.set(http::field::server, "Beast");
                 create_response();
                 break;
+
+            case http::verb::post:
 
             default:
                 // We return responses indicating an error if
                 // we do not recognize the request method.
-                response.result(http::status::bad_request);
-                response.set(http::field::content_type, "text/plain");
-                boost::beast::ostream(response.body())
-                    << "Invalid request-method '" << std::string(request.method_string()) << "'";
+                m_response.result(http::status::bad_request);
+                m_response.set(http::field::content_type, "text/plain");
+                boost::beast::ostream(m_response.body())
+                    << "Invalid request-method '" << std::string(m_request.method_string()) << "'";
                 break;
         }
 
@@ -102,35 +100,35 @@ private:
     // Construct a response message based on the program state.
     void create_response()
     {
-        if(request.target() == "/count")
+        if(m_request.target() == "/count")
         {
-            response.set(http::field::content_type, "text/html");
-            boost::beast::ostream(response.body())
+            m_response.set(http::field::content_type, "text/html");
+            boost::beast::ostream(m_response.body())
                 << "<html>\n"
                 << "<head><title>Request count</title></head>\n"
                 << "<body>\n"
                 << "<h1>Request count</h1>\n"
-                << "<p>There have been " << my_program_state::requestcount() << " requests so far.</p>\n"
+                << "<p>There have been " << request_count() << " requests so far.</p>\n"
                 << "</body>\n"
                 << "</html>\n";
         }
-        else if(request.target() == "/time")
+        else if(m_request.target() == "/time")
         {
-            response.set(http::field::content_type, "text/html");
-            boost::beast::ostream(response.body())
+            m_response.set(http::field::content_type, "text/html");
+            boost::beast::ostream(m_response.body())
                 << "<html>\n"
                 << "<head><title>Current time</title></head>\n"
                 << "<body>\n"
                 << "<h1>Current time</h1>\n"
-                << "<p>The current time is " << my_program_state::now() << " seconds since the epoch.</p>\n"
+                << "<p>The current time is " << now() << " seconds since the epoch.</p>\n"
                 << "</body>\n"
                 << "</html>\n";
         }
         else
         {
-            response.result(http::status::not_found);
-            response.set(http::field::content_type, "text/plain");
-            boost::beast::ostream(response.body()) << "File not found\r\n";
+            m_response.result(http::status::not_found);
+            m_response.set(http::field::content_type, "text/plain");
+            boost::beast::ostream(m_response.body()) << "invalid path\r\n";
         }
     }
 
@@ -140,14 +138,14 @@ private:
         auto self = shared_from_this();
 
         // response.set(http::field::content_length, response.body().size());
-        std::string response_size = std::to_string(response.body().size());
-        response.set(http::field::content_length, response_size);
+        std::string response_size = std::to_string(m_response.body().size());
+        m_response.set(http::field::content_length, response_size);
 
-        http::async_write(socket_, response,
+        http::async_write(m_socket, m_response,
                           [self](boost::beast::error_code ec, std::size_t)
                           {
-                              self->socket_.shutdown(tcp::socket::shutdown_send, ec);
-                              self->deadline_.cancel();
+                              auto return_ec = self->m_socket.shutdown(tcp::socket::shutdown_send, ec);
+                              self->m_deadline.cancel();
                           });
     }
 
@@ -156,13 +154,13 @@ private:
     {
         auto self = shared_from_this();
 
-        deadline_.async_wait(
+        m_deadline.async_wait(
             [self](boost::beast::error_code ec)
             {
                 if(!ec)
                 {
                     // Close socket to cancel any outstanding operation.
-                    self->socket_.close(ec);
+                    auto return_ec = self->m_socket.close(ec);
                 }
             });
     }
@@ -180,33 +178,22 @@ void http_server(tcp::acceptor& acceptor, tcp::socket& socket)
                           });
 }
 
-int main(int argc, char* argv[])
+int main()
 {
     try
     {
-        // Check command line arguments.
-        if(argc != 3)
-        {
-            std::cerr << "Usage: " << argv[0] << " <address> <port>\n";
-            std::cerr << "  For IPv4, try:\n";
-            std::cerr << "    receiver 0.0.0.0 80\n";
-            std::cerr << "  For IPv6, try:\n";
-            std::cerr << "    receiver 0::0 80\n";
-            return EXIT_FAILURE;
-        }
+        const boost::asio::ip::address address = boost::asio::ip::make_address("0.0.0.0");
+        unsigned short port = static_cast<unsigned short>(80U);
 
-        const auto address = boost::asio::ip::make_address(argv[1]);
-        unsigned short port = static_cast<unsigned short>(std::atoi(argv[2]));
-
-        boost::asio::io_context ioc { 1 };
+        boost::asio::io_context io_context { 1 };
 
         tcp::acceptor acceptor {
-            ioc, { address, port }
+            io_context, { address, port }
         };
-        tcp::socket socket { ioc };
+        tcp::socket socket { io_context };
         http_server(acceptor, socket);
 
-        ioc.run();
+        io_context.run();
     }
     catch(const std::exception& e)
     {
