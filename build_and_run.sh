4e9e95817d1f816948a1ebd3ec40ef07ace2a01b
Ubuntu <ubuntu@ip-172-31-2-203.ap-south-1.compute.internal>
2024-11-13 16:58:09 +0000

    added root checking feature in scripts

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
 set -e
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 
 cmake -B build
 cd build
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 set -e
 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
    exit 1
 fi
+
+apt update
 
 apt install -y          \
     g++                 \
